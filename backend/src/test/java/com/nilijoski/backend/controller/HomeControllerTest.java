6e9aca36abc24d05af459b53d29ce5530d9ca7e0
Niko Ilijoski <n_ilijoski@outlook.de>
2025-12-18 00:47:56 +0100

    Bugfix: Dependencies

--- backend/src/test/java/com/nilijoski/backend/controller/HomeControllerTest.java
+++ backend/src/test/java/com/nilijoski/backend/controller/HomeControllerTest.java
@@ -3,16 +3,17 @@ package com.nilijoski.backend.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(HomeController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class HomeControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
